de91c451891476f58538d7745b6997da0855c304
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-04-10 22:53:08 +0600

    feat: integrate Tailwind CSS and create button component
    - Updated index.css to include Tailwind CSS and custom styles for dark mode.
    - Modified TypeScript configuration to support path aliases.
    - Enhanced Vite configuration to resolve path aliases for easier imports.
    - Added components.json for ShadCN UI configuration.
    - Created a basic App component in src/app/index.tsx.
    - Developed a reusable Button component with variants and sizes in src/components/ui/button.tsx.
    - Implemented utility function for class name merging in src/lib/utils.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 import webExtension from "vite-plugin-web-extension";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [webExtension(), react()],
+  plugins: [webExtension(), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });

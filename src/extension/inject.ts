5f25255b75db1b347d066d56247060eae1e822f5
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-04-13 18:54:45 +0600

    fix: ensure enable flag defaults to true if not explicitly set in message payload

--- src/extension/inject.ts
+++ src/extension/inject.ts
@@ -5,7 +5,10 @@ import { mediaDevicePatcher } from "../utils/media-device-patcher";
 window.addEventListener("message", (event) => {
   if (event?.data?.type !== MessageTypeEnum.SETTINGS) return;
 
-  const enable = !!event.data?.payload?.enable;
+  const enable =
+    typeof event.data?.payload?.enable === "boolean"
+      ? event.data?.payload?.enable
+      : true;
   const sourceDeviceLabel = event.data?.payload?.cameraSource?.label;
   const config = event.data?.payload?.config;
 
